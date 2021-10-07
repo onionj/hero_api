15ce33a429c58d9c13e99473740f5e3e0d9fcc58
onionj <onionj98@gmail.com>
2021-10-08 00:26:26 +0330

    create heroes endpoint

--- main.py
+++ main.py
@@ -28,3 +28,12 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+
+@app.post("/heroes/")
+def create_hero(hero: Hero):
+    with Session(engine) as session:
+        session.add(hero)
+        session.commit()
+        session.refresh(hero)
+        return hero
